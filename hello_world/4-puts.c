ab1f3c5b8ea1baa639a4eb4475f747564aca5a86
Asu2824 <Asu.adigozal@gmail.com>
2024-10-22 09:29:10 +0000

    change

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This program prints a specific string to the standard output,
+ * demonstrating the use of the puts function with proper grammar.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
 
