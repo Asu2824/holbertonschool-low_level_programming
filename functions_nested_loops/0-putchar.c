44b6b42a87e95f60aa9f52bb56e275f1a4be00ea
root <Asu.adigozel@gmail.com git config --global user.name Asu2824git push>
2024-10-24 09:16:37 +0000

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Print _putchar
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n'); // Print a new line
+
+    return 0; // Return 0
+}
