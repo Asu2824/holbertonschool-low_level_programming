4f8c43372b0489d3dcb0f9901065c4a494f54a8b
root <Asu.adigozel@gmail.com git config --global user.name Asu2824git push>
2024-10-24 09:19:44 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "holberton.h"
 
-int main() {
-    // Print _putchar
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n'); // Print a new line
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[9] = "Holberton";
+	int i;
 
-    return 0; // Return 0
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
 }
