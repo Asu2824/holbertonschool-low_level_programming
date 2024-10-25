908537c25e7ce6c3bb4d52c573975f3a622ca49a
Asu2824 <Asu.adigozel@gmail.com>
2024-10-25 08:44:43 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,34 +1,19 @@
-#include <unistd.h>
-
+#include "main.h"
 
 /**
- * _putchar -writes a character to stdout
- * @c: character to print
- * Return: 1 on success, -1 on error
- */
-
-int _putchar(char c)
-{
-
-	 return (write(1, &c, 1));
-}
-
- /**
- * main - Entry point
- * Return: Always 0
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	 _putchar('_');
-	 _putchar('p');
-	 _putchar('u');
-	 _putchar('t');
-	 _putchar('c');
-	 _putchar('h');
-	 _putchar('a');
-	 _putchar('r');
-	 _putchar('\n');
-	
-	 return (0);
-}
+	char a[] = "_putchar\n";
 
+	int b = 0;
+
+	while (a[b] != '\0')
+	{
+		_putchar(a[b]);
+		b++;
+	}
+	return (0);
+}
