095b6a962bb1a8c981bcaddc55217e4cbd447d5d
Asu2824 <Asu.adigozalova@gmail.com>
2024-11-11 10:43:14 +0000

    pointers

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,37 +11,32 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    char *start_haystack;
-    char *start_needle;
+	char *start_haystack;
+	char *start_needle;
 
-    /* If the needle is empty, return the haystack string */
-    if (*needle == '\0')
-    {
+if (*needle == '\0')
+	{
         return haystack;
-    }
+	}
 
-    /* Iterate through haystack string */
-    while (*haystack)
-    {
-        start_haystack = haystack;
-        start_needle = needle;
+	while (*haystack)
+	{
+	start_haystack = haystack;
+	start_needle = needle;
+	while (*haystack && *needle && *haystack == *needle)
+	{
+		haystack++;
+		needle++;
+	}
 
-        /* Compare characters of haystack and needle */
-        while (*haystack && *needle && *haystack == *needle)
-        {
-            haystack++;
-            needle++;
-        }
 
-        /* If the entire needle was found, return the starting pointer */
-        if (*needle == '\0')
-        {
-            return start_haystack;
-        }
+	if (*needle == '\0')
+	{
+		return start_haystack;
+	}
 
-        haystack = start_haystack + 1;
-        needle = start_needle;
-    }
-
-    /* Return NULL if 
-
+	haystack = start_haystack + 1;
+	needle = start_needle;
+	}
+return(NULL);
+}
