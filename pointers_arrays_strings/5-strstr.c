8d0af87afc1e69d89d605f1f7de9f8440bd28299
Asu2824 <Asu.adigozalova@gmail.com>
2024-11-11 10:33:36 +0000

    pointers

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,29 +2,46 @@
 #include <stdio.h>
 
 /**
- * _strstr -  a function that locates a substring.
- * @haystack: an input string to search in
- * @needle: an input string to locate into string haystack
- * Return:  a pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+ * _strstr - Locates a substring.
+ * @haystack: The string to search in.
+ * @needle: The substring to locate.
+ *
+ * Return: A pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *startn = needle, *starth = haystack;
+    char *start_haystack;
+    char *start_needle;
 
-	while (*haystack)
-	{
-		starth = haystack;
-		needle = startn;
-		while (*haystack == *needle)
-		{
-			haystack++;
-			needle++;
-		}
+    /* If the needle is empty, return the haystack string */
+    if (*needle == '\0')
+    {
+        return haystack;
+    }
 
-		if (*needle == '\0')
-			return (haystack);
-		haystack = starth + 1;
-	}
-	return (NULL);
-}
+    /* Iterate through haystack string */
+    while (*haystack)
+    {
+        start_haystack = haystack;
+        start_needle = needle;
+
+        /* Compare characters of haystack and needle */
+        while (*haystack && *needle && *haystack == *needle)
+        {
+            haystack++;
+            needle++;
+        }
+
+        /* If the entire needle was found, return the starting pointer */
+        if (*needle == '\0')
+        {
+            return start_haystack;
+        }
+
+        haystack = start_haystack + 1;
+        needle = start_needle;
+    }
+
+    /* Return NULL if 
+
