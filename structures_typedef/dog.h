55de8f7e57a54df74dd0b44c71c528dbdf464684
Asu2824 <asu.adigozel@gmail.com>
2024-11-22 15:05:21 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,8 @@
+#ifndef DOG_H
+#define DOG_H
+#include<stdio.h>
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+#endif
