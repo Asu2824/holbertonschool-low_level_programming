c49ca19b35b034cc9e679fde03fdd6decd6f8397
Asu2824 <asu.adigozel@gmail.com>
2024-11-22 13:00:48 +0000

    malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
 		return (NULL);
-	for (; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
@@ -27,6 +27,6 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 		for (j=0; j < width; j++)
-			array[i][j] = 0;
+		free array[i][j] = 0;
 	return (array);
 }
