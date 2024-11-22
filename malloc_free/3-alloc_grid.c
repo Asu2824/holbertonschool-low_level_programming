8657fc3e77f74cba87c38131e800f9c38ad19cc7
Asu2824 <asu.adigozel@gmail.com>
2024-11-22 13:13:38 +0000

    malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -6,48 +6,48 @@
  * 2-dimensional array of integers.
  * @width: An input integer at number of columns.
  * @height: An input integer at number of rows.
- * 
+ *
  * Return: pointer to a 2D array, NULL on failure.
  */
 int **alloc_grid(int width, int height)
 {
-    int **array;
-    int i = 0, j = 0;
+	int **array;
+	int i = 0, j = 0;
 
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 
-    array = (int **)malloc(sizeof(int *) * height);
-    if (array == NULL)
-    {
-        return (NULL);
-    }
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 
-    for (; i < height; i++)
-    {
-        array[i] = (int *)malloc(sizeof(int) * width);
-        if (array[i] == NULL)
-        {
-            /* Free previously allocated memory */
-            for (j = 0; j < i; j++)
-            {
-                free(array[j]);
-            }
-            free(array);
-            return (NULL);
-        }
-    }
+	for (; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			/* Free previously allocated memory */
+			for (j = 0; j < i; j++)
+			{
+				free(array[j]);
+			}
+			free(array);
+			return (NULL);
+		}
+	}
 
-    for (i = 0; i < height; i++)
-    {
-        for (j = 0; j < width; j++)
-        {
-            array[i][j] = 0; /* Initialize memory to 0 */
-        }
-    }
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0; /* Initialize memory to 0 */
+		}
+	}
 
-    return (array);
+	return (array);
 }
 
